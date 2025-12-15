d3de06d806b07f8d78abec953dde5dc9b63ca95e
thanhhai-house <haivothanh0603@gmail.com>
2025-12-15 18:56:25 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const parts = [
+  {
+    sku: "PT-LOC-001",
+    name: "Lọc dầu động cơ",
+    price: 85000,
+    stock: 24,
+    compatible: ["Honda", "Yamaha", "Suzuki"]
+  },
+  {
+    sku: "PT-PH-002",
+    name: "Bố thắng đĩa trước",
+    price: 155000,
+    stock: 12,
+    compatible: ["Honda", "Yamaha"]
+  },
+  {
+    sku: "PT-DN-003",
+    name: "Dầu nhớt 10W-40",
+    price: 128000,
+    stock: 40,
+    compatible: ["Honda", "Yamaha", "Suzuki"]
+  }
+];
